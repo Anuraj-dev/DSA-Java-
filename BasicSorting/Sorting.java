c5f758e2402fd1b1fa8b9d2afe61fd19961cb9e4
Anuraj Jit Saikia <rajasaikia1644@gmail.com>
2025-09-30 23:03:00 +0530

    Day 29

--- BasicSorting/Sorting.java
+++ BasicSorting/Sorting.java
@@ -75,6 +75,30 @@ public class Sorting {
         return arr;
     }
 
+    //? Counting sort (In trip letss goo)
+    public static int[] countingSort(int arr[]){
+        int maxNo = Integer.MIN_VALUE;
+        //Calculate Largest integer
+        for(int i = 0; i<arr.length; i++){
+            maxNo = Math.max(maxNo, arr[i]);
+        }
+        //Frequency array
+        int freq[] = new int[maxNo + 1];
+        for(int i = 0; i<arr.length; i++){
+            freq[arr[i]]++;
+        }
+
+        //Sort it or say print it
+        int j = 0;
+        for(int i = 0; i<arr.length; i++){
+            while(freq[i]>0){
+                arr[j] = i;
+                j++;
+                freq[i]--;
+            }
+        }
+        return arr;
+    }
     
 
     //?For printing array
@@ -88,6 +112,7 @@ public class Sorting {
     public static void main(String[] args) {
         Integer arr[] = {5,4,1,3,2};
         int arr2[] = {5,4,1,3,2};
+        int arr3[] = {1,4,1,3,2,4,3,7};
         //? Bubble sort testing
         // printArr(bubbleSort(arr));
 
@@ -98,10 +123,13 @@ public class Sorting {
         // printArr(insertionSort(arr));
 
         //? Mine Insertion sort
-        printArr(insertionSort(arr2));
+        // printArr(insertionSort(arr2));
 
         //? Inbuilt java sort
         Arrays.sort(arr, Collections.reverseOrder());             //? Inbuilt sort like Arrays.sort(arr, startingIndex, endingIndex(nonInclusive)) 
         // printArr(arr);
+
+        //? Counting sort
+        printArr(countingSort(arr3));
     }
 }
