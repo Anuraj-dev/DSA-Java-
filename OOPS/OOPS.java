475d91ce84bb85194390b80e305b083751bb9185
Anuraj Jit Saikia <rajasaikia1644@gmail.com>
2025-10-22 22:40:10 +0530

    Day 51

---
+++ OOPS/OOPS.java
@@ -0,0 +1,28 @@
+package OOPS;
+public class OOPS {
+    
+    public static void main(String[] args) {
+        Pen p1 = new Pen();
+        p1.setColor("red");
+        System.out.println(p1.color);
+        p1.setTip(5);
+        System.out.println(p1.tip);
+        p1.color = "Yellow";
+        System.out.println(p1.color);
+    }
+}
+
+class Pen{
+    String color;
+    int tip;
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
+
+
